a537997a49623b82e00f75193875addc21dae907
hbzhu <hbzhu@thoughtworks.com>
2020-04-01 15:52:19 +0800

    [feat] add deploy script.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Archguard local deploy script
+
+- Start all containers
+`./deploy.sh up`
+
+- Stop all containers
+`./deploy.sh stop`
+
+- Remove all containers
+`./deploy.sh rm`
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+op=$1
+
+if [ "$op" == 'up' ] 
+then
+    docker-compose up -d    
+elif [ "$op" == 'stop' ]
+then
+    docker-compose stop
+elif [ "$op" == 'rm' ]
+then
+    docker-compose rm
+else   
+    echo 'Not support operation $op !!'
+fi
